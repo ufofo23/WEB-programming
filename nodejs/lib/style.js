a5b2c6a1e76b6c375e715314d93064b3234d173f
ufofo23 <142997521+ufofo23@users.noreply.github.com>
2023-12-08 18:19:10 +0900

    Delete nodejs directory

--- nodejs/lib/style.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-exports.css = (request, response)=>{
-    fs.readFile("./style.css", 'utf8', function(err, data){
-        response.writeHead(200);
-        response.write(data);
-        response.end();
-    })
-}
